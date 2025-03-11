4bfb095563d1e425a3c92bd8ff07d6382b7acd0e
Azat <kushkarov.azat@gmail.com>
2025-03-11 21:29:14 +0300

    first commit

---
+++ src/main/java/ru/azat/TaskPlannerFrontendService/TaskPlannerFrontendServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.azat.TaskPlannerFrontendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerFrontendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerFrontendServiceApplication.class, args);
+	}
+
+}
